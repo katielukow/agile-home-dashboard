74bb06427597f605860b7650ecd7127383494cf7
Katie Lukow <caitlin.lukow@gmail.com>
2025-01-23 15:34:59 +0000

    selected time changed to slider

--- Pages/Kettle_Pricing.py
+++ Pages/Kettle_Pricing.py
@@ -1,17 +1,29 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 import plotly.graph_objects as go
 # from Home import api_key
 
 
 # Function to get the current or selected time
-def get_current_time(toggle):
+def get_current_time(toggle, df):
     if toggle:
-        selected_time = st.time_input("Select time:", "12:00")
+        df = df[df["valid_from"].dt.date == datetime.now(pytz.UTC).date()]
+        start_time = df["valid_from"].min().to_pydatetime()
+        end_time = df["valid_from"].max().to_pydatetime()
+
+        # Use the slider
+        selected_time = st.slider(
+            "Select time:",
+            min_value=start_time,
+            max_value=end_time,
+            value=start_time,
+            format="HH:mm",
+            step=timedelta(minutes=30),
+        )
         combined_datetime = datetime.combine(
-            datetime.now(pytz.UTC).date(), selected_time
+            datetime.now(pytz.UTC).date(), selected_time.time()
         )
         return pd.to_datetime(pytz.UTC.localize(combined_datetime))
     return datetime.now(pytz.UTC)
@@ -22,12 +34,16 @@ def calculate_kettle_cost(df, current_time, run_time, power):
     current_cost_row = df[
         (df["valid_from"] <= current_time) & (df["valid_to"] > current_time)
     ]
+    current_price = current_cost_row.iloc[0]["value_inc_vat"]
     if current_cost_row.empty:
         return None, None, None, None
 
-    next_cost_row = df.iloc[df.index.get_loc(current_cost_row.index[0]) + 1]
-    current_price = current_cost_row.iloc[0]["value_inc_vat"]
-    next_price = next_cost_row["value_inc_vat"]
+    if current_cost_row.index[0] == df.index[-1]:
+        next_cost_row = current_cost_row
+        next_price = 0
+    else:
+        next_cost_row = df.iloc[df.index.get_loc(current_cost_row.index[0]) + 1]
+        next_price = next_cost_row["value_inc_vat"]
 
     cost_now = ((run_time / 3600) * current_price * power) / 100
     cost_next = ((run_time / 3600) * next_price * power) / 100
@@ -46,29 +62,81 @@ def calculate_kettle_cost(df, current_time, run_time, power):
 def display_kettle_costs(
     current_price, next_price, cost_now, cost_next, current_cost_row, next_cost_row
 ):
-    st.write(f"Current energy cost: £{current_price:.4f} p/kWh")
-    st.write(f"Next energy cost: £{next_price:.4f} p/kWh")
-
-    st.write(
-        f"The kettle costs £{cost_now:.4f} until {current_cost_row.iloc[0]['valid_to'].strftime('%H:%M')}."
-    )
-
-    color = "green" if cost_next <= cost_now else "red"
     st.markdown(
-        f'<p style="color:{color};">The kettle will cost £{cost_next:.4f} '
-        f"from {next_cost_row['valid_from'].strftime('%H:%M')} "
-        f"to {next_cost_row['valid_to'].strftime('%H:%M')}. </p>",
+        """
+        <div style="text-align: center;">
+            <strong>Current Energy Cost</strong><br>
+            <span style="font-size: 1.5em; color: black;">{:.4f} p/kWh</span>
+        </div>
+        """.format(current_price),
         unsafe_allow_html=True,
     )
+
+    # Next energy cost
+    st.markdown(
+        """
+        <div style="text-align: center; margin-top: 20px;">
+            <strong>Next Energy Cost</strong><br>
+            <span style="font-size: 1.5em; color: black;">{:.4f} p/kWh</span>
+        </div>
+        """.format(next_price),
+        unsafe_allow_html=True,
+    )
+
+    # Kettle current cost
+    st.markdown(
+        """
+        <div style="text-align: center; margin-top: 30px;">
+            <strong>Kettle Current Cost</strong><br>
+            <span style="font-size: 1.2em; color: black;">£{:.4f}</span><br>
+            <small>Valid until {}</small>
+        </div>
+        """.format(cost_now, current_cost_row.iloc[0]["valid_to"].strftime("%H:%M")),
+        unsafe_allow_html=True,
+    )
+
+    # Kettle next cost with color coding
+    color = "green" if cost_next <= cost_now else "red"
+    if next_price == 0:
+        color = "black"
+        st.markdown(
+            """
+        <div style="text-align: center; margin-top: 30px;">
+            <strong>Kettle Next Cost</strong><br>
+            <span style="font-size: 1.2em; color: {};">£{:.4f}</span><br>
+            <small>The next kettle cost is not available</small>
+        </div>
+        """.format(
+                color,
+                cost_next,
+            ),
+            unsafe_allow_html=True,
+        )
+    else:
+        st.markdown(
+            """
+            <div style="text-align: center; margin-top: 30px;">
+                <strong>Kettle Next Cost</strong><br>
+                <span style="font-size: 1.2em; color: {};">£{:.4f}</span><br>
+                <small>From {} to {}</small>
+            </div>
+            """.format(
+                color,
+                cost_next,
+                next_cost_row["valid_from"].strftime("%H:%M"),
+                next_cost_row["valid_to"].strftime("%H:%M"),
+            ),
+            unsafe_allow_html=True,
+        )
 
 
 # Function to plot kettle timing
 def plot_kettle_timing():
     kettle_timing = pd.DataFrame(
         {
-            "Volume [mL]": [600, 550, 350, 1100, 637],
-            "Time [s]": [137, 135, 98, 237, 148],
-            "Starting Temp [C]": [18, 18, 12, 12, 15],
+            "Volume [mL]": [600, 550, 350, 1100, 637, 804, 600],
+            "Time [s]": [137, 135, 98, 237, 148, 178, 150],
+            "Starting Temp [C]": [18, 18, 12, 12, 15, 11, 16],
         }
     )
 
@@ -103,12 +171,12 @@ def main():
         "Kettle run time [s]:", min_value=0, max_value=2000, value=137
     )
     power = st.number_input(
-        "Kettle power [kW]:", min_value=0.0, max_value=3.0, value=2.1
+        "Kettle power [kW]:", min_value=0.0, max_value=1e8, value=2.1
     )
 
     # Toggle for manual time selection
     toggle = st.toggle("Select time manually", False)
-    current_time = get_current_time(toggle)
+    current_time = get_current_time(toggle, st.session_state.df)
 
     # Calculate and display costs
     (
@@ -132,6 +200,7 @@ def main():
     else:
         st.write("No pricing data available for the current time.")
 
+    st.markdown("##")
     # Plot kettle timing
     plot_kettle_timing()
 
