2be9a398b1158880a95285a09f8a5e6421d6f465
Katie Lukow <caitlin.lukow@gmail.com>
2025-01-28 15:31:21 +0000

    corrected oven calculations to the minute, reformat costs

--- pages/Oven_Pricing.py
+++ pages/Oven_Pricing.py
@@ -20,7 +20,7 @@ def display_oven_costs(current_price, next_price, cost_now, current_cost_row):
                 border-radius: 10px;
                 box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
                 <strong style="font-size: 1.2em;">Current Energy Cost</strong><br>
-                <span style="font-size: 1.8em;">{current_price:.4f} p/kWh</span>
+                <span style="font-size: 1.4em;">{current_price:.4f} p/kWh</span>
             </div>
             """,
             unsafe_allow_html=True,
@@ -37,7 +37,7 @@ def display_oven_costs(current_price, next_price, cost_now, current_cost_row):
                     border-radius: 10px;
                     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
                     <strong style="font-size: 1.2em;">Current Energy Cost</strong><br>
-                    <span style="font-size: 1.8em;">{next_price:.4f} p/kWh</span>
+                    <span style="font-size: 1.4em;">{next_price:.4f} p/kWh</span>
                 </div>
                 """,
             unsafe_allow_html=True,
@@ -54,7 +54,7 @@ def display_oven_costs(current_price, next_price, cost_now, current_cost_row):
                     border-radius: 10px;
                     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
                     <strong style="font-size: 1.2em;">Current Oven Cost</strong><br>
-                    <span style="font-size: 1.8em;">£{cost_now:.4f}</span>
+                    <span style="font-size: 1.4em;">£{cost_now:.4f}</span>
                 </div>
                 """,
             unsafe_allow_html=True,
@@ -67,6 +67,27 @@ def calculate_kettle_cost(current_price, next_price, run_time, power):
     return cost_now, cost_next
 
 
+st.markdown(
+    """
+    <style>
+    /* Style for the number input label */
+    label[for="total-bake-time"] {
+        font-size: 1.5em; /* Increase label font size */
+        font-weight: bold; /* Make label bold */
+    }
+
+    /* Style for the number input box */
+    div[data-baseweb="input"] input {
+        font-size: 1.1em; /* Increase font size inside the input box */
+        padding: 10px; /* Add padding for a larger input field */
+        height: 60px; /* Set height for larger appearance */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
 def main():
     st.title("Oven Pricing on Octopus Agile")
 
@@ -74,9 +95,22 @@ def main():
         toggle = st.toggle("Select time manually", False)
         current_time = get_current_time(toggle, st.session_state.df)
 
-        bake_time = st.number_input("Total Bake Time [min]:", value=20)
+        st.markdown(
+            f"""
+                <div style="
+                    color: {st.session_state.font};
+                    border-radius: 10px;
+                    padding-bottom: 10px;">
+                    <span style="font-size: 1.1em;">Total bake time [min]:</span><br>
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+        bake_time = st.number_input(
+            "Total bake time [min]: ", value=20, label_visibility="collapsed"
+        )
         oven_power = 2.5  # kW
-        current_price, next_price, current_cost_row, next_cost_row = get_current_cost(
+        current_price, next_price, current_cost_row, _ = get_current_cost(
             st.session_state.df, current_time
         )
         time_to_next_half_hour = 30 - current_time.minute % 30
