a073cf54034c730bbc2758be1d3a5d2b1aa7cfe6
Brady Planden <55357039+BradyPlanden@users.noreply.github.com>
2025-01-31 09:35:56 +0000

    feat: adds kettle model with efficiency parameter fitting (#18)

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# [Unreleased](https://github.com/katielukow/agile-home-dashboard)
+
+## Features
+
+- [#18](https://github.com/katielukow/agile-home-dashboard/pull/18) - Adds predictive kettle model with efficiency fitting scripts
+
+## Bug Fixes
+
+## Breaking Changes
---
+++ utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from scipy.optimize import minimize
+
+# Kettle observations
+kettle_timing = pd.DataFrame(
+    {
+        "Volume [mL]": [600, 550, 350, 1100, 637, 804, 600, 570, 500, 830, 627],
+        "Time [s]": [137, 135, 98, 237, 148, 178, 150, 125, 130, 205, 153],
+        "Starting Temp [C]": [18, 18, 12, 12, 15, 11, 16, 13, 12, 10, 15],
+    }
+)
+
+
+def kettle_energy(init_temp, cp, m, kappa):
+    """First-principles kettle model"""
+    return (100.0 - init_temp) * cp * m * kappa
+
+
+@st.cache_data
+def fit_kettle_efficiency():
+    """
+    Identifies kettle efficiency parameter `kappa`
+
+    Returns
+    -------
+    kappa : float
+    """
+    temperature = kettle_timing["Starting Temp [C]"].mean()
+    volume_ml = kettle_timing["Volume [mL]"].to_numpy() / 1000
+    time_s = kettle_timing["Time [s]"].to_numpy()
+
+    def kettle_fit(kappa):
+        W = kettle_energy(temperature, cp, volume_ml, kappa)
+        residuals = W / 2.1 - time_s
+        return np.sum(residuals**2)
+
+    result = minimize(kettle_fit, x0=np.array([1.0]))
+    return result.x[0]
+
+
+# Defaults
+cp = 4.19298  # mean value between 0C - 100C
+kappa = fit_kettle_efficiency()
