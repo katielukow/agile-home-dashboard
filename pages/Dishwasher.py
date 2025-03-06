43daf090b9f0044dd172785b1b8098cd413880dd
Katie Lukow <caitlin.lukow@gmail.com>
2025-03-06 09:12:56 +0000

    removed unused lines of code

--- pages/Dishwasher.py
+++ pages/Dishwasher.py
@@ -31,20 +31,6 @@ def calculate_drying_cost(dishwasher_time, df, current_time):
         current_price, _, _, _ = get_current_cost(df, current_time)
         total_cost += (current_price * remaining_time) / 6000
 
-    # st.write(f"Total cost: £{total_cost:.2f}")
-
-    # # Define the rolling window size
-    # window_size = int(dishwasher_time / 30)  # 1 hour = 2 intervals (30 min each)
-    # costs["rolling_sum"] = costs["value_inc_vat"].rolling(window=window_size).sum()
-    # st.write(window_size)
-    # # Find the minimum cost index and end time
-    # min_index = costs["rolling_sum"].idxmin()
-    # end_time = costs.loc[min_index, "valid_to"]
-
-    # # Calculate the hours remaining from now until the optimal end time
-    # time_diff = end_time - current_time
-    # end_at = round(time_diff.total_seconds() / 3600, 1)
-
     return total_cost
 
 
@@ -81,7 +67,6 @@ def main():
         try:
             hours, minutes = map(int, dishwasher_time.split(":"))
             dishwasher_time = hours * 60 + minutes
-            # st.write(f"Total time dishwasher runs: {dishwasher_time} minutes")
         except ValueError:
             st.error("Invalid time format. Please use HH:MM format.")
 
@@ -105,34 +90,7 @@ def main():
         delay = dish_costs.loc[min_cost_loc]["time"] - current_time
 
         st.write(f"Delay the dishwasher: {delay.total_seconds() / 3600} hours")
-        # if current_time is not None:
 
-        #     # dry_time = st.number_input("Drying run time [hr]:", value=3.0)
-        #     power = st.number_input("Washing machine power [kW]:", value=2.0)
-
-        #     forward_df = st.session_state.df[
-        #         st.session_state.df["valid_from"]
-        #         > current_time
-        #     ]
-
-        #     if st.session_state.df["valid_from"].max().date() != dtime.now(
-        #         pytz.UTC
-        #     ).date() + timedelta(days=1):
-        #         st.warning("Data for tomorrow not available yet.")
-
-        #     if (
-        #         current_time + timedelta(minutes=dishwasher_time)
-        #         > st.session_state.df["valid_from"].max()
-        #     ):
-        #         st.warning("Not enough data available for the selected washing time.")
-        #     else:
-        #         end_time, end_at = calculate_drying_cost(
-        #             dishwasher_time, forward_df, current_time
-        #         )
-        #         display_washer_timing(end_time, end_at)
-
-        # else:
-        #     st.markdown("##")
     else:
         st.error("API key not found.")
 
